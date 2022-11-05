050f0c8c9e3e15d62102b67d99de8e12ba462fcc
shin <heoconlinhat123@gmail.com>
2022-11-05 20:56:35 +0700

    i forget to export that

--- src/common/domain/base-classes/index.ts
+++ src/common/domain/base-classes/index.ts
@@ -2,3 +2,4 @@ export * from './entity.base';
 export * from './decorator.base';
 export * from './value-object.base';
 export * from './aggregate-root.base';
+export * from './domain-service.base';
