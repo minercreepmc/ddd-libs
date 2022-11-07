e19ebea255c251d1717b634130221f0ce7129e5f
shin <heoconlinhat123@gmail.com>
2022-11-07 16:42:03 +0700

    fix

--- src/common/domain/value-objects/id/id.value-object.ts
+++ src/common/domain/value-objects/id/id.value-object.ts
@@ -1,10 +1,6 @@
 import { ValueObject } from '@domain/base-classes';
 
 export abstract class ID extends ValueObject<string> {
-  get value(): string {
-    return this.props.value;
-  }
-
   protected constructor(value: string) {
     super({ value });
   }
