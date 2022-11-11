f15fe050b8eb18f4d32a4d5801173dd330fcf939
shin <heoconlinhat123@gmail.com>
2022-11-11 08:07:32 +0700

    fix miss commit

--- src/common/domain/domain-events/index.ts
+++ src/common/domain/domain-events/index.ts
@@ -2,4 +2,4 @@ export * from './implementation';
 export * from './domain-event-publisher.interface';
 export * from './aggregate-domain-event-publisher.abstract';
 export * from './domain-event.abstract';
-export * from './domain-event-handler.abstract'
+export * from './domain-event-handler.abstract';
--- src/common/domain/value-objects/id/id.value-object.ts
+++ src/common/domain/value-objects/id/id.value-object.ts
@@ -1,4 +1,4 @@
-import { ValueObject } from "@domain/value-object.abstract";
+import { ValueObject } from '@domain/value-object.abstract';
 
 export abstract class ID extends ValueObject<string> {
   protected constructor(value: string) {
