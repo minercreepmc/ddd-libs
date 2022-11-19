f5c3ad13f5a7b3e4258f29b6e8c78b6ae947710e
shin <heoconlinhat123@gmail.com>
2022-11-19 20:48:31 +0700

    fix index not export domain-services

--- src/common/domain/index.ts
+++ src/common/domain/index.ts
@@ -1,5 +1,6 @@
 export * from './driven-ports';
 export * from './domain-events';
+export * from './domain-services';
 export * from './value-objects';
 export * from './aggregate-root.abstract';
 export * from './entity.abstract';
