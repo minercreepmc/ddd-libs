530e683bbf99374b3f22b22ed5feb2d7fb60989b
shin <heoconlinhat123@gmail.com>
2022-11-25 08:24:28 +0700

    Fix shit

--- src/common/domain/value-objects/id/uuid.value-object.ts
+++ src/common/domain/value-objects/id/uuid.value-object.ts
@@ -4,7 +4,8 @@ import { ArgumentInvalidExeception } from '@tinphamm/common-exceptions';
 
 export class UUID extends ID {
   static create(id?: string): UUID {
-    if (id && !this.isValid(id)) throw new ArgumentInvalidExeception('Invalid uuid');
+    if (id && !this.isValid(id))
+      throw new ArgumentInvalidExeception('Invalid uuid');
     const uuid = new UUID(id ? id : uuidV4());
     return uuid;
   }
