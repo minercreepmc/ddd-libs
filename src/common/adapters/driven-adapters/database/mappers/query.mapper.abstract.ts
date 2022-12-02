7dc86c01ceae7a0445dc3346ee862b735916c6b4
shin <heoconlinhat123@gmail.com>
2022-12-02 19:25:45 +0700

    props -> details

--- src/common/adapters/driven-adapters/database/mappers/query.mapper.abstract.ts
+++ src/common/adapters/driven-adapters/database/mappers/query.mapper.abstract.ts
@@ -1,12 +1,16 @@
 import { IBaseEntity } from '@domain/entities';
 import { FindOptionsWhere } from 'typeorm';
 
-export type QueryParams<AggregateDetails> = Partial<IBaseEntity & AggregateDetails>;
+export type QueryParams<AggregateDetails> = Partial<
+  IBaseEntity & AggregateDetails
+>;
 
 export type WhereClause<OrmModel> =
   | FindOptionsWhere<OrmModel>
   | FindOptionsWhere<OrmModel>[];
 
 export abstract class AbstractQueryMapper<AggregateDetails, OrmModel> {
-  abstract toQuery(params: QueryParams<AggregateDetails>): WhereClause<OrmModel>;
+  abstract toQuery(
+    params: QueryParams<AggregateDetails>
+  ): WhereClause<OrmModel>;
 }
