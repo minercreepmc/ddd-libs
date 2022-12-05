dc733607db7f8d036cbae55a7b74f9ec8fb68f64
shin <heoconlinhat123@gmail.com>
2022-12-05 21:40:42 +0700

    Add utils function

--- src/common/utils/functions/index.ts
+++ src/common/utils/functions/index.ts
@@ -1,1 +1,2 @@
 export * from './is-array-contain-instance-of';
+export * from './is-between-length';
---
+++ src/common/utils/functions/is-between-length.ts
@@ -0,0 +1,3 @@
+export function isStringBetweenLength(value: string, min: number, max: number): boolean {
+  return min <= value.length && value.length <= max;
+}
