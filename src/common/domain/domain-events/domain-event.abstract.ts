bdff3af22a813d8228706744c57e8d0cc926dad9
shin <heoconlinhat123@gmail.com>
2022-12-14 18:58:54 +0700

    Update event

--- src/common/domain/domain-events/domain-event.abstract.ts
+++ src/common/domain/domain-events/domain-event.abstract.ts
@@ -1,26 +1,18 @@
 import { DateVO, ID, UUID } from '@domain/value-objects';
-import { GenericObject } from 'src/types/object.type';
 
 export type DomainEventClass<T> = new (args: any) => DomainEvent<T>;
-export type DomainEventDetails = GenericObject;
 
-export abstract class DomainEvent<AggregateClassType> {
+export abstract class DomainEvent<DomainEventDetails> {
   readonly eventId: ID;
-  readonly aggregateId: ID;
   readonly dateOccurred: DateVO;
-  readonly eventName: string;
-  readonly aggregateType: AggregateClassType;
   abstract readonly details: DomainEventDetails;
 
   constructor(
-    aggregateId: ID,
-    aggregateType: AggregateClassType,
-    eventName: string
+    readonly aggregateId: ID,
+    readonly aggregateType: string,
+    readonly eventName: string
   ) {
     this.eventId = UUID.create();
     this.dateOccurred = DateVO.now();
-    this.aggregateId = aggregateId;
-    this.eventName = eventName;
-    this.aggregateType = aggregateType;
   }
 }
