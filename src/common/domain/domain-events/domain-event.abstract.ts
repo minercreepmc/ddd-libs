f67639d32d9f188fd48936049b12a17dcd8b2b6e
shin <heoconlinhat123@gmail.com>
2022-12-17 20:02:08 +0700

    details now standalize in abstract

--- src/common/domain/domain-events/domain-event.abstract.ts
+++ src/common/domain/domain-events/domain-event.abstract.ts
@@ -1,25 +1,27 @@
 import { DateVO, ID, UUID } from '@domain/value-objects';
 
 export type DomainEventClass<T> = new (args: any) => DomainEvent<T>;
-export interface DomainEventProps {
+export interface DomainEventProps<DomainEventDetails> {
   aggregateId: ID;
   aggregateType: string;
   eventName: string;
+  details: DomainEventDetails;
 }
 
 export abstract class DomainEvent<DomainEventDetails> {
   readonly eventId: ID;
   readonly dateOccurred: DateVO;
-  abstract readonly details: DomainEventDetails;
+  readonly details: DomainEventDetails;
   readonly aggregateId: ID;
   readonly aggregateType: string;
   readonly eventName: string;
 
-  constructor(eventProps: DomainEventProps) {
+  constructor(eventProps: DomainEventProps<DomainEventDetails>) {
     this.eventId = UUID.create();
     this.dateOccurred = DateVO.now();
     this.aggregateId = eventProps.aggregateId;
     this.aggregateType = eventProps.aggregateType;
     this.eventName = eventProps.eventName;
+    this.details = eventProps.details;
   }
 }
