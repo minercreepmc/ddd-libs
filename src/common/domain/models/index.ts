d08c0e2fe80a8070eab308e179bc37f04ff5ddee
shin <heoconlinhat123@gmail.com>
2023-01-01 13:51:49 +0700

    [models]: add read model abstract

---
+++ src/common/domain/models/index.ts
@@ -0,0 +1,1 @@
+export * from './read-model.abstract';
---
+++ src/common/domain/models/read-model.abstract.ts
@@ -0,0 +1,19 @@
+export interface IReadModelData {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  details: any;
+}
+
+export abstract class ReadModel {
+  readonly id: string;
+  readonly createdAt: Date;
+  updatedAt: Date;
+  readonly details: any;
+  constructor({ id, details, createdAt, updatedAt }: IReadModelData) {
+    this.id = id;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.details = details;
+  }
+}
