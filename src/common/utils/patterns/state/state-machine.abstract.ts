92297a2bab81ccedcc2cb11b8bdc9bd4b4e8822d
shin <heoconlinhat123@gmail.com>
2023-01-11 19:55:55 +0700

    [state]: entity now can have state

--- src/common/utils/patterns/state/state-machine.abstract.ts
+++ src/common/utils/patterns/state/state-machine.abstract.ts
@@ -1,7 +1,7 @@
 import { AbstractAggregateRoot } from '@domain/aggregates';
 
 export interface IState<T extends AbstractAggregateRoot<any>> {
-  aggregate: T;
+  entity: T;
 }
 
 export interface IStateMachine<T extends IState<any>> {
