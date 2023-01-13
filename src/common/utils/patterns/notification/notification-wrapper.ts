29ee8a5f83b27c1a13247bacb4abefbac3fac4e9
shin <heoconlinhat123@gmail.com>
2023-01-13 20:12:41 +0700

    [notification]: remove abstract check

--- src/common/utils/patterns/notification/notification-wrapper.ts
+++ src/common/utils/patterns/notification/notification-wrapper.ts
@@ -2,7 +2,6 @@ import { Notification } from './notification';
 
 export abstract class AbstractNotificationWrapper<NoteType> {
   protected readonly note = new Notification<NoteType>();
-  abstract check(candidate: any): void | Promise<void>;
   isValid() {
     return !this.note.hasNote();
   }
