9f1e06ba654b2025220311dfb9bb999ef73e5b8a
shin <heoconlinhat123@gmail.com>
2023-01-30 12:34:25 +0700

    [dto]: fix

--- src/common/adapters/driver-adapters/dtos/response.dto.abstract.ts
+++ src/common/adapters/driver-adapters/dtos/response.dto.abstract.ts
@@ -2,4 +2,10 @@ export class AbstractResponseDto {
   id: string;
   createdAt: Date;
   updatedAt: Date;
+
+  constructor(data: AbstractResponseDto) {
+    this.id = data.id;
+    this.createdAt = data.createdAt;
+    this.updatedAt = data.updatedAt;
+  }
 }
