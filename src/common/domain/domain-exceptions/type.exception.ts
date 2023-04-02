ac817a7c8c9ebea847b21d4f2f1dd9b49a0fb8eb
shin <heoconlinhat123@gmail.com>
2023-04-02 08:57:25 +0700

    add invalid argument exception

--- src/common/domain/domain-exceptions/type.exception.ts
+++ src/common/domain/domain-exceptions/type.exception.ts
@@ -4,6 +4,7 @@ export enum TypeExceptionCodes {
   argumentNotANumberException = 'TYPE.EXCEPTION.ARGUMENT_NOT_A_NUMBER_EXCEPTION',
   argumentNotAStringException = 'TYPE.EXCEPTION.ARGUMENT_NOT_A_STRING_EXCEPTION',
   argumentIsEmptyException = 'TYPE.EXCEPTION.ARGUMENT_IS_EMPTY_EXCEPTION',
+  argumentInvalidException = 'TYPE.EXCEPTION.ARGUMENT_INVALID_EXCEPTION',
 }
 
 export class ArgumentNotANumberException extends ValidationExceptionBase {
@@ -22,4 +23,10 @@ export class ArgumentIsEmptyException extends ValidationExceptionBase {
   readonly name = ArgumentIsEmptyException.name;
   code = TypeExceptionCodes.argumentIsEmptyException;
   message = 'Argument is empty';
+}
+
+export class ArgumentInvalidException extends ValidationExceptionBase {
+  readonly name = ArgumentInvalidException.name;
+  code = TypeExceptionCodes.argumentInvalidException;
+  message = 'Argument is invalid';
 }
